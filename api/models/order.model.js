989ed2d5267856ab0717fde1729581384c77460e
KieuTrinh-T <96128479+KieuTrinh-T@users.noreply.github.com>
2023-05-12 12:42:58 +0700

    order change

--- api/models/order.model.js
+++ api/models/order.model.js
@@ -53,8 +53,15 @@ const getOrderByUser = async(req, res) => {
             console.log('Mess from View: Connected to MongoDB');
         });
         const user_id = req.params.id
-        const result = await Order.find({ user: user_id })
-        return res.status(200).json(result)
+        const result = await Order.find({ user: user_id }).populate({
+            path: 'orderItems',
+            populate: {
+                path: 'product',
+                select: { 'product_name': 1, 'price': 1, 'original_price': 1, 'thumb': 1 }
+            }
+        }).sort({ 'dateOrdered': -1
+        })
+        return res.status(200).json(convertArrayResult(result))
     } catch (err) {
         return res.status(500).json(err)
     }
