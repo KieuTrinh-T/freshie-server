43560e076dd208f61a32b3390b09574e5650b3b8
KieuTrinh-T <96128479+KieuTrinh-T@users.noreply.github.com>
2023-05-12 22:16:38 +0700

    add new domain

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ require('dotenv').config()
 const MONGODB_URI = process.env.MONGODB_URI;
 // use it before all route definitions
 // app.use(cors({ origin: 'http://localhost:4200' }));
-const allowedOrigins = ['http://localhost:4200', 'https://freshie-server-mqt8rgu0d-kieutrinh-t.vercel.app'];
+const allowedOrigins = ['http://localhost:4200', 'https://freshie-server-mqt8rgu0d-kieutrinh-t.vercel.app', 'http://www.freshie.live'];
 
 app.use(cors({
     origin: function(origin, callback) {
